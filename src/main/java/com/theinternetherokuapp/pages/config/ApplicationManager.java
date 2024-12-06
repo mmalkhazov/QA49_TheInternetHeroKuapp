6cd863feab89936f70e38295cb9edd0d3895aeb3
mmalkhazov <mmalkhazov@yahoo.com>
2024-12-06 19:22:51 +0100

    Homework_27

---
+++ src/main/java/com/theinternetherokuapp/pages/config/ApplicationManager.java
@@ -0,0 +1,48 @@
+package com.theinternetherokuapp.pages.config;
+
+import com.theinternetherokuapp.utils.MyListener;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+
+import java.time.Duration;
+
+public class ApplicationManager {
+
+    String browser;
+    public WebDriver driver;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+
+    public WebDriver startTest() {
+
+        if (browser.equals("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equals("firefox")) {
+            driver = new FirefoxDriver();
+        } else if (browser != null &&
+                !browser.equals("chrome") &&
+                !browser.equals("firefox")) {
+            throw new IllegalArgumentException("Browser entered is not correct");
+        }
+
+        driver = new EventFiringDecorator(new MyListener()).decorate(driver);
+
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
+    }
+
+    public void stopTest() {
+        driver.quit();
+    }
+}
+
+
+
+
--- src/test/java/com/theinternetherokuapp/tests/TestBase.java
+++ src/test/java/com/theinternetherokuapp/tests/TestBase.java
@@ -1,28 +1,37 @@
 package com.theinternetherokuapp.tests;
 
+import com.theinternetherokuapp.pages.config.ApplicationManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
-import java.time.Duration;
+import java.lang.reflect.Method;
 
 public class TestBase {
-    WebDriver driver;
+
+    protected ApplicationManager app = new ApplicationManager(System.getProperty("browser", "chrome"));
+    public WebDriver driver;
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeMethod
-    public void init() {
-        driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void init(Method method) {
+        driver = app.startTest();
+        logger.info("Start test: " + method.getName());
 
     }
 
-    @AfterMethod (enabled=false)
-    public void tearDown(){
-        driver.quit();
-
+    @AfterMethod(enabled = true)
+    public void tearDown(ITestResult result) {
+        if (result.isSuccess()) {
+            logger.info("Test result: PASSED " + result.getMethod().getMethodName());
+        } else {
+            logger.info("Test result:FAILED " + result.getMethod().getMethodName());
+        }
+        logger.info("*******************************************");
+        app.stopTest();
     }
 
 }
